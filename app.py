c360345514a990bf652dfb8c0e0cefd6db90c760
Christian Cruz <christian.cruz92@hotmail.com>
2023-12-12 10:01:24 -0500

    created model for user and first route

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "Secret!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 
 connect_db(app)
-db.create_all()
+
+def before_first_request():
+    db.create_all()
+
+@app.route('/')
+def index():    
+    return "Blogly app is running!"
+
--- models.py
+++ models.py
@@ -1,1 +1,21 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Site user"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    image_url = db.Column(db.String(150), nullable = False, default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTArDP_bnVwv1r88E7xDWOt6utBQv1HdPsensjcjjtwfg&s')
