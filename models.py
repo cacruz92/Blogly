51a7fba0c1495085862bd91ad40d68fea4a56470
Christian Cruz <christian.cruz92@hotmail.com>
2023-12-20 22:42:34 -0500

    added Models for Tag and PostTag with through relationship

--- models.py
+++ models.py
@@ -30,4 +30,24 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
     user = db.relationship('User', backref='posts')
+
+    tags = db.relationship('Tag', secondary='post_tags', backref='posts')
+
+class Tag(db.Model):
+    """Tags for posts"""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+
+class PostTag(db.Model):
+    """Mapping of posts with tags"""
+
+    __tablename__ = "post_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id', primary_key=True))
+
