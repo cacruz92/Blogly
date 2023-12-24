09f47d051a8a1ae0197e0379c876ae36078991db
Christian Cruz <christian.cruz92@hotmail.com>
2023-12-24 08:28:47 -0500

    added tag-post cross functionality by updating the add, edit and show post templates to include tags

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def delete_user(user_id):
 def show_new_post_form(user_id):    
     """Get new post form"""
     user = User.query.get_or_404(user_id)
-    return render_template("newpostform.html", user=user)
+    tags = Tag.query.all()
+    return render_template("newpostform.html", user=user, tags=tags)
 
 
 @app.route('/users/<int:user_id>/posts/new', methods=["POST"])
@@ -97,7 +98,11 @@ def add_post(user_id):
     title = request.form["title"]
     content = request.form["content"]
 
-    new_post = Post(title=title, content=content, user=user)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
+
+    new_post = Post(title=title, content=content, user=user, tags=tags)
     db.session.add(new_post)
     db.session.commit()
 
@@ -116,6 +121,9 @@ def edit_post(post_id):
     post = Post.query.get_or_404(post_id)
     post.title = request.form["title"]
     post.content = request.form["content"]
+
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
     
     db.session.add(post)
     db.session.commit()
@@ -126,7 +134,8 @@ def edit_post(post_id):
 def show_edit_user_form(post_id):
     """Populates the form that allows you to edit a particular post"""
     post = Post.query.get_or_404(post_id)
-    return render_template("editpost.html", post=post)
+    tags = Tag.query.all()
+    return render_template("editpost.html", post=post, tags=tags)
 
 @app.route('/posts/<int:post_id>/delete', methods=["POST"])
 def delete_post(post_id):
